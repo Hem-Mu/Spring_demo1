c1a4fb10660857e4c4bc6dddf2ea274493bee5af
Hem-Mu <kimmim1223@gmail.com>
2022-08-23 16:53:09 +0900

    SpringDataJpaMemberRepository 추가
    Test Code 실패함.

---
+++ src/main/java/hamster/demo/repository/Member/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package hamster.demo.repository.Member;
+
+import hamster.demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
